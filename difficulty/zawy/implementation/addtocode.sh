fc1cbce30dfd9dd6e43b1f825ddf7153d52faed2
HashToBeWild <hashtobewild@cryptocurrency.ninja>
2019-04-23 14:30:59 -0500

    Updated zawy to use base file anchors

--- difficulty/zawy/implementation/addtocode.sh
+++ difficulty/zawy/implementation/addtocode.sh
@@ -18,16 +18,16 @@ DestinationFile=$BasePath'/configure.ac'
 # Only do this, if the file does not contain the KeyPhrase
 if [ -z $(grep "$KeyPhrase" "$DestinationFile") ]; then 
     FragmentFile='./fragments/configure.ac.001'
-    MarkerLineText='AC_LANG_PUSH'
+    MarkerLineText='Bitcoin_Modules_Included_End'
     SearchParameters='/'$MarkerLineText'/r '$FragmentFile
     # Insert before the marker
     sed -n -i -e "$SearchParameters" -e 1x -e '2,${x;p}' -e '${x;p}' $DestinationFile 
 
     FragmentFile='./fragments/configure.ac.002'
-    MarkerLineText='Options used to compile and link'
+    MarkerLineText='Bitcoin_Modules_Report_List_End'
     SearchParameters='/'$MarkerLineText'/r '$FragmentFile
-    # Insert after the marker
-    sed -i -e "$SearchParameters" $DestinationFile
+    # Insert before the marker
+    sed -n -i -e "$SearchParameters" -e 1x -e '2,${x;p}' -e '${x;p}' $DestinationFile 
 else
     echo "Skipping $DestinationFile"
 fi
@@ -39,13 +39,13 @@ DestinationFile=$BasePath'/src/Makefile.am'
 # Only do this, if the file does not contain the KeyPhrase
 if [ -z $(grep "$KeyPhrase" "$DestinationFile") ]; then 
     FragmentFile='./fragments/Makefile.am.001'
-    MarkerLineText='obj\/build\.h\:\ FORCE'
+    MarkerLineText='Bitcoin_Modules_Core_Headers'
     SearchParameters='/'$MarkerLineText'/r '$FragmentFile
     # Insert before the marker
     sed -n -i -e "$SearchParameters" -e 1x -e '2,${x;p}' -e '${x;p}' $DestinationFile 
 
     FragmentFile='./fragments/Makefile.am.002'
-    MarkerLineText='\#\ wallet\:\ shared\ between'
+    MarkerLineText='Bitcoin_Modules_Server_Sources'
     SearchParameters='/'$MarkerLineText'/r '$FragmentFile
     # Insert before the marker
     sed -n -i -e "$SearchParameters" -e 1x -e '2,${x;p}' -e '${x;p}' $DestinationFile 
